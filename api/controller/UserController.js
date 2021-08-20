6cdecc4a113430894db726f4a550ada69801188f
Thanhloc <Nguyenloc12021999vn@gmail.com>
2021-08-20 22:25:55 +0700

    s

--- api/controller/UserController.js
+++ api/controller/UserController.js
@@ -37,13 +37,68 @@ const UserCtrl = {
     get: async (req, res) => {
         try {
             const user = await Users.find().select('-password')
-
+            if(!user) return res.status(400).json({msg: "Người dùng không tồn tại !"})
             res.json({user})
         } catch (err) {
 
         }
 
-    }
+    },
+    refreshToken: (req, res) =>{
+        try {
+            const rf_token = req.cookies.refreshtoken;
+           // res.json({rf_token})
+          if(!rf_token) return res.status(400).json({msg: "Please Login or Register"})
+
+          jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) =>{
+                if(err) return res.status(400).json({msg: "Please Login or Register"})
+
+                const accesstoken = createAccessToken({id: user.id})
+
+                res.json({accesstoken})
+        })
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message}) 
+        }
+        
+    },
+    login :async(req,res)=>{
+        try {
+            const {email, password} = req.body;
+
+            const user = await Users.findOne({email})
+            if(!user) return res.status(400).json({msg: "Người dùng không tồn tại."})
+
+            const isMatch = await bcrypt.compare(password, user.password)
+            if(!isMatch) return res.status(400).json({msg: "Mật khẩu không đúng."})
+
+            // If login success , create access token and refresh token
+            const accesstoken = createAccessToken({id: user._id})
+            const refreshtoken = createRefreshToken({id: user._id})
+
+            res.cookie('refreshtoken', refreshtoken, {
+                httpOnly: true,
+                path: 'api/user/refresh_token',
+                maxAge: 7*24*60*60*1000 // 7d
+            })
+            
+            res.json({accesstoken} )
+
+            
+        } catch (error) {
+            return res.status(500).json({msg: error.message})
+        }
+    },
+    getUser : async(req,res)=>{ 
+        try {
+          const user =await Users.findById(req.user.id).select("-password")
+          if(!user) return res.status(400).json({msg: "Người dùng không tồn tai!"})
+          res.json(user)
+        } catch (error) {
+            return res.status(500).json({msg: error.message})
+        }
+      },
 }
 const createAccessToken = (user) => {
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '11m' })
--- api/routes/UserRouter.js
+++ api/routes/UserRouter.js
@@ -1,8 +1,15 @@
 const router= require("express").Router()
 const UserCtrl = require("../controller/UserController")
-
+const auth = require("../middlware/auth")
 router.get('/get',UserCtrl.get)
 
 router.post('/add',UserCtrl.addUser)
+
+
+router.get('/refresh_token',UserCtrl.refreshToken)
+
+router.post('/login',UserCtrl.login)
+
+router.get('/get_user',auth,UserCtrl.getUser)
 
 module.exports=router
